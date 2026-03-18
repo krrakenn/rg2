fd1bcdca25b8bbc5056799ac9880819ec8673297
Raghav Shankar <66262040+krrakenn@users.noreply.github.com>
2026-03-18 11:38:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
-'''import os
+import os
 def get_secret(key):
     try:
         import streamlit as st
         return st.secrets[key]
     except:
         return os.getenv(key)
-'''
-import os
-import json
 
-def get_secret(key):
-    try:
-        import streamlit as st
-        val = st.secrets[key]
-    except:
-        val = os.getenv(key)
+# import os
+# import json
 
-    # Parse JSON if it's a string
-    if isinstance(val, str):
-        try:
+# def get_secret(key):
+#     try:
+#         import streamlit as st
+#         val = st.secrets[key]
+#     except:
+#         val = os.getenv(key)
+
+#     # Parse JSON if it's a string
+#     if isinstance(val, str):
+#         try:
             val = json.loads(val)
         except json.JSONDecodeError:
             pass
